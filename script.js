6656bed6c877251af7aec6b367fc6072ebfd47bd
Cleiton <cleiton.v.pinheiro@gmail.com>
2023-11-01 16:36:33 -0300

    :tada: modificando o css

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function toggleMode() {
 
   const img = document.querySelector("#profile img")
   if(html.classList.contains('light')) {
-    img.setAttribute('src', './assets/assets/avatar-light.png')
+    img.setAttribute('src', './img/avatar-light.png')
 
   } else {
-    img.setAttribute('src', './assets/assets/avatar.png')
+    img.setAttribute('src', './img/avatar.png')
   }
   
  // if(html.classList.contains('light')) {
